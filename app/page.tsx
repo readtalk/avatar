dce05403b0a0f926a4c216867a209b6d60f17b66
READTalk <redtalk.app@hotmail.com>
2026-03-09 16:14:21 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
         href="https://vercel.com/templates/next.js/blob-starter"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercel</p>
+        <p>Next</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Blob on Vercel
+        Avatar
       </h1>
       <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
         <Uploader />
@@ -26,14 +26,14 @@ export default function Home() {
           href="https://vercel.com/blob"
           className="font-medium underline underline-offset-4 hover:text-black transition-colors"
         >
-          Vercel Blob
+          READTalk
         </Link>{' '}
         demo. Built with{' '}
         <Link
           href="https://nextjs.org/docs"
           className="font-medium underline underline-offset-4 hover:text-black transition-colors"
         >
-          Next.js App Router
+          App Router
         </Link>
         .
       </p>
